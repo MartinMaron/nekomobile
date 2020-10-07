5d4c055eebce2d30ea358a897a391348c9926f9f
MartinMaron <martin.maron@e-neko.de>
2020-10-07 18:51:41 +0200

    Testy

--- app/src/main/java/de/eneko/nekomobile/activities/detail/Messgeraete/MessgeraetBaseActivity.java
+++ app/src/main/java/de/eneko/nekomobile/activities/detail/Messgeraete/MessgeraetBaseActivity.java
@@ -2,6 +2,7 @@ package de.eneko.nekomobile.activities.detail.Messgeraete;
 
 
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
