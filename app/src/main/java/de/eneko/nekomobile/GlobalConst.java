18f80860e63cc652aff2353b60209777c022ff54
Martin Maron <45365815+MartinMaron@users.noreply.github.com>
2022-12-05 06:46:31 +0100

    techniczny

--- app/src/main/java/de/eneko/nekomobile/GlobalConst.java
+++ app/src/main/java/de/eneko/nekomobile/GlobalConst.java
@@ -14,7 +14,7 @@ public class GlobalConst {
     public static final String PATH_SONTEX_OLD = "//storage/emulated/0/Supercom/roads";
     public static final String PATH_EXIM = "//storage/emulated/0/Exim";
 
-    public static final String NEKOMOBILE_USER = "mariusz";
+    public static final String NEKOMOBILE_USER = "martin";
     // Service wlaczamy i wylaczamy w konstruktorze NekoDropBox
 
     public static final Integer DROPBOX_SERVICE_INTERVALL = 3600000;   // 1 Stunde
