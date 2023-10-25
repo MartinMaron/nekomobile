17190dce24d97f08d4a7b2d8dad7058eb4319d6f
Martin Maron <45365815+MartinMaron@users.noreply.github.com>
2023-10-25 07:41:55 +0200

    techniczny

--- app/src/main/java/de/eneko/nekomobile/GlobalConst.java
+++ app/src/main/java/de/eneko/nekomobile/GlobalConst.java
@@ -15,7 +15,7 @@ public class GlobalConst {
     public static final String PATH_SONTEX_OLD = "//storage/emulated/0/Supercom/roads";
     public static final String PATH_EXIM = "//storage/emulated/0/Exim";
 
-    public static final String NEKOMOBILE_USER = "michal";
+    public static final String NEKOMOBILE_USER = "mariuszStab";
     // Service wlaczamy i wylaczamy w konstruktorze NekoDropBox
 
     public static final Integer DROPBOX_SERVICE_INTERVALL = 3600000;   // 1 Stunde
@@ -26,7 +26,6 @@ public class GlobalConst {
     public static final String SONTEX_SAVEDBY_SWVERSION = "3.0.4";
     public static final String SONTEX_ROAD_VERSION = "6.3";
     public static final String SONTEX_longDateTimeFormat = "yyyy-MM-dd HH:mm:ss";
-
     public static final String SONTEX_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJkZjJkMmE0Zi0yZGJmLTQwYjMtODljZi05MWRlYzI3ZTViZDkifQ.eyJpYXQiOjE2Njk5Njc4ODMsImp0aSI6IjVmY2M1YjdiLTA5NWEtNGRlMy1hNWI2LTgwOTlmMjIxZDU4MCIsImlzcyI6Imh0dHBzOi8vc3NvLmV4Y2hhbmdlLXBsYXRmb3JtLmFwcC9hdXRoL3JlYWxtcy9zb25leGEiLCJhdWQiOiJodHRwczovL3Nzby5leGNoYW5nZS1wbGF0Zm9ybS5hcHAvYXV0aC9yZWFsbXMvc29uZXhhIiwic3ViIjoiOTJmMzM3MTQtNzEyMS00OTZjLWEwNTMtZjE5NzhiZGJjY2ZiIiwidHlwIjoiT2ZmbGluZSIsImF6cCI6Im5naW54LWFwaSIsInNlc3Npb25fc3RhdGUiOiIxNDczMzRkNy1hMzU3LTRmZjctODhkOC1mM2UxNGU1MGViYTIiLCJzY29wZSI6ImVtYWlsIHByb2ZpbGUgb2ZmbGluZV9hY2Nlc3MiLCJzaWQiOiIxNDczMzRkNy1hMzU3LTRmZjctODhkOC1mM2UxNGU1MGViYTIifQ.Uoe_GI38sAucGSpMCt0H3g-x5dNPFeDKcmAZ6HSTIGU";
 
 }
